0986bf6d50f9e676a9a3028d46e210d531efec11
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-22 18:52:11 -0800

    Middleware function catches unkown routes

--- index.js
+++ index.js
@@ -95,6 +95,12 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
+// Middelware function for catching requests made to non-existent routes
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
